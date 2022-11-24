fe32e913466b81e2f371199aba4f612f37b9b4ea
David7Arciniegas <david7arciniegas@gmail.com>
2022-11-24 12:43:15 -0500

    added first sort function

--- src/Users.js
+++ src/Users.js
@@ -1,11 +1,30 @@
 import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
-const baseURL = "https://jsonplaceholder.typicode.com/users";
 
 function Users() {
-
+  const baseURL = "https://jsonplaceholder.typicode.com/users";
   const [users, setUsers] = useState(null);
+  const [sorted, setSorted] = useState({ sorted: "name", reversed: false });
+
+  const sortByName = () => {
+    setSorted({ sorted: "name", reversed: !sorted.reversed });
+    const usersCopy = [...users];
+    usersCopy.sort((userA, userB) => {
+      const nameA = userA.name;
+      const nameB = userB.name;
+
+      if (sorted.reversed) {
+        return nameB.localeCompare(nameA);
+      }
+      return nameA.localeCompare(nameB);
+    });
+    setUsers(usersCopy);
+  };
+
+  const sortByCity = () => {};
+
+  const sortByUserName = () => {};
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
@@ -17,37 +36,43 @@ function Users() {
   if (!users) return null;
 
   const renderUsers = () => {
-		return users.map((user) => {
-			return (
-				<tr>
-					<td>{user.id}</td>
-					<td>{`${user.name}`}</td>
-					<td>{user.email}</td>
-					<td>{user.gender}</td>
-				</tr>
-			);
-		});
-	};
+    return users.map((user) => {
+      return (
+        <tr>
+          <td>{user.id}</td>
+          <td>{`${user.name}`}</td>
+          <td>{user.username}</td>
+          <td>{user.address.city}</td>
+        </tr>
+      );
+    });
+  };
 
   return (
-		<div className="App">
-<div className="table-container">
-  <table>
-    <thead>
-      <tr>
-        <th><span>id</span></th>
-        <th><span>Name</span></th>
-        <th><span>Email</span></th>
-        <th><span>Gender</span></th>
-      </tr>
-    </thead>
-    <tbody>{renderUsers()}</tbody>
-  </table>
-
-</div>
+    <div className="App">
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <span>Id</span>
+              </th>
+              <th onClick={sortByName}>
+                <span>Nombre</span>
+              </th>
+              <th>
+                <span>Nombre de Usuario</span>
+              </th>
+              <th>
+                <span>Ciudad</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>{renderUsers()}</tbody>
+        </table>
+      </div>
     </div>
-	);
+  );
 }
-
 
 export default Users;
